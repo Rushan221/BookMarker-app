e4370c3461b636e5bc41298c9f7ac5b80bab4889
Rushan <rushan2072@gmail.com>
2019-01-02 14:53:30 +0545

    Delete function added

--- js/main.js
+++ js/main.js
@@ -9,6 +9,12 @@ function saveBookmark(e) {
   //console.log(siteName);
   //console.log(siteUrl);
 
+  //validation for empty form
+  if (!siteName || siteUrl) {
+    alert("Please fill in the form");
+    return false;
+  }
+
   var bookmark = {
     name: siteName,
     url: siteUrl
@@ -22,8 +28,65 @@ function saveBookmark(e) {
     bookmarks.push(bookmark);
     //set to local storage
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  } else {
+    //fetch from localstorage
+    var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+    //adding to the array
+    bookmarks.push(bookmark);
+    //reset it to local storage
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   }
+
+  fetchBookmarks();
 
   //Prevent form from submitting
   e.preventDefault();
+}
+
+//fetch bookmarks
+
+function fetchBookmarks() {
+  //getting bookmarks from local storage
+  var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+  console.log(bookmarks);
+
+  //get outputid
+  var bookmarksResults = document.getElementById("bookmarksResults");
+
+  //build output
+  bookmarksResults.innerHTML = "";
+  for (var i = 0; i < bookmarks.length; i++) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML +=
+      '<div class="jumbotron">' +
+      "<h3>" +
+      name +
+      '   <a class="btn btn-primary" target="_blank" href="' +
+      url +
+      '">Visit </a>' +
+      "   <a onclick=\"deleteBookmark('" +
+      url +
+      '\')" class="btn btn-danger" href="#">Delete </a>' +
+      "</h3>" +
+      "</div>";
+  }
+}
+
+function deleteBookmark(url) {
+  //get bookmarks from local storage
+  var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+  for (var i = 0; i < bookmarks.length; i++) {
+    if (bookmarks[i].url == url) {
+      bookmarks.splice(i, 1);
+    }
+  }
+  //reset local storage
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+  //refetch bookmarks
+  fetchBookmarks();
 }
