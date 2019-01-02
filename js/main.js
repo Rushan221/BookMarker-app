a0bd9a077016ee9b46e42cabacfeb8f7f7f0482e
Rushan <rushan2072@gmail.com>
2019-01-02 13:28:37 +0545

    getting and setting vlues in local storage

--- js/main.js
+++ js/main.js
@@ -4,11 +4,25 @@ document.getElementById("myForm").addEventListener("submit", saveBookmark);
 
 function saveBookmark(e) {
   //Get form values
-  var siteName = document.getElementById("sietName").value;
-  var siteUrl = documet.getElementById("siteUrl").value;
+  var siteName = document.getElementById("siteName").value;
+  var siteUrl = document.getElementById("siteUrl").value;
+  //console.log(siteName);
+  //console.log(siteUrl);
 
-  console.log(siteName);
-  console.log(siteUrl);
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  };
+  //console.log(bookmark);
+
+  //Test for already added bookmarks
+  if (localStorage.getItem("bookmarks") === null) {
+    //bookmarks array
+    var bookmarks = [];
+    bookmarks.push(bookmark);
+    //set to local storage
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
 
   //Prevent form from submitting
   e.preventDefault();
